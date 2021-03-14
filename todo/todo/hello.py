f897446f877d85a9cf8228ea16c6ca0ba61ba265
davidjoliver86 <davidjoliver86@gmail.com>
2021-03-14 11:05:18 -0700

    flask app updates

--- todo/todo/hello.py
+++ todo/todo/hello.py
@@ -1,6 +1,12 @@
+import os
+
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+PORT = os.environ.get("FLASK_RUN_PORT", 5000)
+
+
+@app.route("/")
 def hello():
-    return 'Server started in port NNNN'
+    return f"Server started in port {PORT}"
